998ba1394860a2f21e30c3034d9710c365e95a42
Siraj <asrf@C02SG255G8WN.local>
2019-03-25 11:35:35 +0000

    Remaining code

--- app/src/main/java/com/example/firebaseauthentication/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.firebaseauthentication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
     EditText editTextNumber, editTextVerificationCode;
     Button sendSMSButton, sendVerificationButton;
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback;
+    String verificationCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
 
-        editTextNumber = findViewById(R.id.edit_phone_number);
+        editTextNumber = findViewById(R.id.editTextNumber);
         editTextVerificationCode = findViewById(R.id.editTextVerificationCode);
         sendSMSButton = findViewById(R.id.sendSMSButton);
         sendVerificationButton = findViewById(R.id.sendVerificationButton);
@@ -38,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
         //initialize callback
-        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        mCallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
 
@@ -52,11 +60,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                 super.onCodeSent(s, forceResendingToken);
+                //when verification code will be sent, this method will be called
+                verificationCode = s;
+                Toast.makeText(MainActivity.this, "Code is sent to the number", Toast.LENGTH_SHORT).show();
             }
         };
     }
 
     public void send_sms(View v){
+        //called when send SMS button will be clicked
+        String number = editTextNumber.getText().toString();
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number, 60, TimeUnit.SECONDS,this,mCallback
+        );
+    }
 
+    public void signInWithPhone(PhoneAuthCredential credential){
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //user is successfully signed in
+                            Toast.makeText(MainActivity.this, "User signed in successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void verify(View view){
+        String inputCode = editTextVerificationCode.getText().toString();
+        if(verificationCode.equals("")){
+            verifyPhoneNumber(verificationCode, inputCode);
+        }
+    }
+
+    private void verifyPhoneNumber(String verificationCode, String inputCode) {
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, inputCode);
+        signInWithPhone(credential);
     }
 }
